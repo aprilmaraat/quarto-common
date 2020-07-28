7e13c1bb4ef55827866d57c8fdd45921f4d8cea2
April John Maraat <aprilmaraat@gmail.com>
2020-07-28 18:18:42 +0800

    Added models in Quarto.Api.Models

---
+++ Quarto.Api.Models/Listing.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace Quarto.Api.Models
+{
+    public class Listing
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int ParentListing { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string Country { get; set; }
+        public AccommodationType AccomodationTypeID { get; set; }
+        public AvailabilityType AvailabilityTypeID { get; set; }
+
+        [JsonIgnore]
+        public virtual EnumListingAccommodationType EnumListingAccommodationType  { get; set;}
+        [JsonIgnore]
+        public virtual EnumListingAvailabilityType EnumListingAvailabilityType { get; set; }
+    }
+}
