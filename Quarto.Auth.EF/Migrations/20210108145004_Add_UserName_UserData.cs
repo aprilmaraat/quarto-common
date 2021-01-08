265e6df177eeeee838b0cdb701840f9875877d5d
April John Maraat <aprilmaraat@gmail.com>
2021-01-08 22:52:32 +0800

    Updated package version referrence; Added corresponding migrations for the updated model

---
+++ Quarto.Auth.EF/Migrations/20210108145004_Add_UserName_UserData.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Quarto.Auth.EF.Migrations
+{
+    public partial class Add_UserName_UserData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "User.Data",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "User.Data");
+        }
+    }
+}
