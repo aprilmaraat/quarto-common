99da05d81b8e4af3a3b7fb6a2cf69d886a23c90a
April John Maraat <aprilmaraat@gmail.com>
2021-01-27 16:37:50 +0800

    Added generic response models.

---
+++ Quarto.Common.Package/Models/Response/ResponseMessage.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+namespace Quarto.Common.Package
+{
+    /// <summary>
+    /// Possible reponse messages
+    /// </summary>
+    public enum ResponseMessage
+    {
+        /// <summary>
+        /// Response message for success
+        /// </summary>
+        [Description("Success")]
+        Success,
+        /// <summary>
+        /// Response message for failure
+        /// </summary>
+        [Description("Exception")]
+        Exception,
+        /// <summary>
+        /// Response message for other erros
+        /// </summary>
+        MiscError,
+    }
+}
